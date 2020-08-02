022f598d479a84d5a0663e09e95f2e53115e15b1
Rohit Nagraj <41142333+RohitNagraj@users.noreply.github.com>
2020-08-02 17:21:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def sentiment_score(tokenizer = tokenizer, model = model, maxlen=30):
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT"))
-	app.run(host='0.0.0.0',port=port)
+	app.run(host='0.0.0.0',port=port, threaded=False)
